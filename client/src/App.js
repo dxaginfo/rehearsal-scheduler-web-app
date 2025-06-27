bdc24edcfaf77c941ad4c652fdfe68e48830fb77
dxaginfo <142343166+dxaginfo@users.noreply.github.com>
2025-06-26 23:37:44 -0300

    Initial frontend App component.

---
+++ client/src/App.js
@@ -0,0 +1,10 @@
+import React from 'react';
+function App() {
+  return (
+    <div style={{maxWidth: 500, margin: '0 auto', padding: 16}}>
+      <h1>Rehearsal Scheduler</h1>
+      <p>Schedule practices, track attendance, get reminders.</p>
+    </div>
+  );
+}
+export default App;
